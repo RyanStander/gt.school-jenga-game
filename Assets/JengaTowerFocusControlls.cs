e797c2f8f670e8428745295f7beab6319075a6eb
Ryan <stander.ryan@gmail.com>
2023-05-11 23:33:41 +0200

    Buttons for swapping focus

---
+++ Assets/JengaTowerFocusControlls.cs
@@ -0,0 +1,39 @@
+using Cinemachine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JengaTowerFocusControlls : MonoBehaviour
+{
+    [SerializeField] private GameObject _jengaTowerButton;
+    [SerializeField] private float _jengaButtonYOffset = 50;
+
+    private float _currentJengaButtonYOffset;
+
+    public void CreateJengaTowerButton(GameObject jengaTower, string buttonName,
+        CinemachineVirtualCamera cinemachineVirtualCamera)
+    {
+        GameObject jengaTowerButton = Instantiate(_jengaTowerButton, transform);
+        jengaTowerButton.GetComponent<Button>().onClick
+            .AddListener(() => FocusJengaTower(jengaTower, cinemachineVirtualCamera));
+
+        RectTransform rectTransform = jengaTowerButton.GetComponent<RectTransform>();
+        var anchoredPosition = rectTransform.anchoredPosition;
+        anchoredPosition = new Vector2(anchoredPosition.x,
+            anchoredPosition.y - _currentJengaButtonYOffset);
+        rectTransform.anchoredPosition = anchoredPosition;
+
+        _currentJengaButtonYOffset += _jengaButtonYOffset;
+
+        //replace the space in buttonName with a new line
+        buttonName = buttonName.Replace(" ", "\n");
+        
+        jengaTowerButton.GetComponentInChildren<TextMeshProUGUI>().text = buttonName;
+    }
+
+    private void FocusJengaTower(GameObject jengaTower, ICinemachineCamera cinemachineVirtualCamera)
+    {
+        cinemachineVirtualCamera.LookAt = jengaTower.transform;
+        cinemachineVirtualCamera.Follow = jengaTower.transform;
+    }
+}
--- Assets/Scripts/Jenga/JengaSpawner.cs
+++ Assets/Scripts/Jenga/JengaSpawner.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections.Generic;
 using ApiInterpretation;
+using Cinemachine;
 using UnityEngine;
 
 namespace Jenga
 {
     public class JengaSpawner : MonoBehaviour
     {
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        [SerializeField] private JengaTowerFocusControlls _jengaTowerFocusControlls;
+
         [SerializeField] private GameObject _jengaBlockGlass;
         [SerializeField] private GameObject _jengaBlockWood;
         [SerializeField] private GameObject _jengaBlockStone;
@@ -17,23 +22,31 @@ namespace Jenga
         [SerializeField] private float _helveticaXOffset = -0.5f;
         [SerializeField] private float _helveticaZOffset = -2;
         [SerializeField] private float _helveticaScale = 0.02f;
-        
+
 
         private float currentJengaSetHeight;
         private float currentJengaSetRotation;
         private float currentJengaTowerPoint;
 
-        private void Awake()
+        private List<GameObject> _jengaTowers = new();
+
+        private void OnValidate()
+        {
+            if (_jengaTowerFocusControlls == null)
+                _jengaTowerFocusControlls = FindObjectOfType<JengaTowerFocusControlls>();
+        }
+
+        private void Start()
         {
             ApiInterpreter apiInterpreter = new ApiInterpreter();
             apiInterpreter.SetupStandards();
-            
+
             currentJengaTowerPoint = -_newJengaTowerPoint;
-            
+
             CreateJengaTower(apiInterpreter.SixthGradeStandards);
-            
+
             CreateJengaTower(apiInterpreter.SeventhGradeStandards);
-            
+
             CreateJengaTower(apiInterpreter.EighthGradeStandards);
         }
 
@@ -47,19 +60,24 @@ namespace Jenga
             }
 
             GameObject helveticaText = Instantiate(_helveticaTextPrefab);
-            helveticaText.transform.position= new Vector3(currentJengaTowerPoint+_helveticaXOffset, 0, _helveticaZOffset);
+            helveticaText.transform.position =
+                new Vector3(currentJengaTowerPoint + _helveticaXOffset, 0, _helveticaZOffset);
             helveticaText.transform.localScale *= _helveticaScale;
             var text = helveticaText.GetComponent<SimpleHelvetica>();
             text.Text = standards[0].Grade;
             text.GenerateText();
 
-            GameObject jengaTower = new GameObject( standards[0].Grade+" Jenga Tower")
+            GameObject jengaTower = new GameObject(standards[0].Grade + " Jenga Tower")
             {
                 transform =
                 {
                     position = new Vector3(currentJengaTowerPoint, 0, 0),
                 }
             };
+
+            _jengaTowers.Add(jengaTower);
+            
+            _jengaTowerFocusControlls.CreateJengaTowerButton(jengaTower, standards[0].Grade, _camera);
 
             while (standardsQueue.Count > 1)
             {
@@ -77,7 +95,7 @@ namespace Jenga
                 //set the parent of the jengaSet to the jengaTower
                 currentJengaSetHeight += _newJengaSetHeight;
                 currentJengaSetRotation += 90;
-            
+
                 var zPos = -_JengaSetZPoint;
                 for (int i = 0; i < 3; i++)
                 {
@@ -86,25 +104,27 @@ namespace Jenga
                     switch (standard.Mastery)
                     {
                         case 0:
-                            jengaBlock=Instantiate(_jengaBlockGlass,jengaSet.transform);
+                            jengaBlock = Instantiate(_jengaBlockGlass, jengaSet.transform);
                             jengaBlock.transform.localPosition = new Vector3(0, 0, zPos);
                             break;
                         case 1:
-                            jengaBlock=Instantiate(_jengaBlockWood,jengaSet.transform);
+                            jengaBlock = Instantiate(_jengaBlockWood, jengaSet.transform);
                             jengaBlock.transform.localPosition = new Vector3(0, 0, zPos);
                             break;
                         case 2:
-                            jengaBlock=Instantiate(_jengaBlockStone,jengaSet.transform);
+                            jengaBlock = Instantiate(_jengaBlockStone, jengaSet.transform);
                             jengaBlock.transform.localPosition = new Vector3(0, 0, zPos);
                             break;
                     }
+
                     zPos += _JengaSetZPoint;
                 }
             }
+
             currentJengaSetHeight = 0;
             currentJengaSetRotation = 0;
-            
-            currentJengaTowerPoint +=_newJengaTowerPoint;
+
+            currentJengaTowerPoint += _newJengaTowerPoint;
         }
     }
 }
